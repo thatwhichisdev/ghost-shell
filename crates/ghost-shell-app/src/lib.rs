89692e07c4d18bfa59e4cb0f795dbcafbb2807ee
nanobreaker <nan0br3aker@gmail.com>
2026-07-26 11:11:59 +0300

    restructure

--- crates/ghost-shell-app/src/lib.rs
+++ crates/ghost-shell-app/src/lib.rs
@@ -1,55 +1,20 @@
 use anyhow::Result;
 use ghost_shell_config::AppConfig;
-use gpui::{
-    App, DisplayId, Global, KeyBinding, Pixels, PlatformDisplay, Size,
-    WindowBackgroundAppearance, WindowBounds, WindowKind, WindowOptions,
-    actions,
-    layer_shell::{Anchor, KeyboardInteractivity, Layer, LayerShellOptions},
-    point,
-    prelude::*,
-    px,
-};
-use std::rc::Rc;
+use gpui::App;
 
-pub struct BarConfig {
-    pub display_id: DisplayId,
-    pub size: Size<Pixels>,
-}
-
-pub struct AppState {
-    config: AppConfig,
-    bars: Vec<BarConfig>,
-}
+pub struct AppState;
 
 impl AppState {
     #[must_use]
-    pub fn new(
-        config: AppConfig,
-        displays: &[Rc<dyn PlatformDisplay>],
-    ) -> Self {
-        let bars: Vec<BarConfig> = displays
-            .iter()
-            .map(|display| {
-                let display_size = display.bounds().size;
-                BarConfig {
-                    display_id: display.id(),
-                    size: Size::new(display_size.width, px(config.bar_height)),
-                }
-            })
-            .collect();
-
-        Self { config, bars }
+    pub fn new() -> Self {
+        Self
     }
 }
-
-impl Global for AppState {}
-
-actions!(window, [Quit]);
 
 /// Initializes shell
 ///
 /// # Errors
-/// Bubbles up errors from gpui
+/// Bubbles up errors from bar initialization
 pub fn init(cx: &mut App) -> Result<()> {
     let app_config = match ghost_shell_config::load() {
         Ok(config) => config,
@@ -59,59 +24,9 @@ pub fn init(cx: &mut App) -> Result<()> {
         }
     };
 
-    let app_state = AppState::new(app_config.clone(), &cx.displays());
-
-    let windows_options: Vec<WindowOptions> = app_state
-        .bars
-        .iter()
-        .map(|bar| {
-            let app_id: String = format!("ghost-shell-{:?}", bar.display_id);
-            let namespace: String = format!("namespace-{:?}", bar.display_id);
-            WindowOptions {
-                window_bounds: Some(WindowBounds::Windowed(gpui::Bounds {
-                    origin: point(px(0.0), px(0.0)),
-                    size: bar.size,
-                })),
-                titlebar: None,
-                focus: false,
-                show: true,
-                kind: WindowKind::LayerShell(LayerShellOptions {
-                    namespace,
-                    layer: Layer::Top,
-                    anchor: Anchor::TOP | Anchor::LEFT | Anchor::RIGHT,
-                    exclusive_zone: Some(px(app_state
-                        .config
-                        .bar_exclusive_zone)),
-                    keyboard_interactivity: KeyboardInteractivity::OnDemand,
-                    ..Default::default()
-                }),
-                is_movable: true,
-                app_owns_titlebar_drag: false,
-                is_resizable: true,
-                is_minimizable: true,
-                display_id: Some(bar.display_id),
-                window_background: WindowBackgroundAppearance::Blurred,
-                app_id: Some(app_id),
-                window_min_size: None,
-                window_decorations: None,
-                icon: None,
-                tabbing_identifier: None,
-            }
-        })
-        .collect();
-
-    for window_options in windows_options {
-        cx.open_window(window_options, |_, cx| {
-            cx.new(|_| ghost_shell_bar::Bar)
-        })?;
+    for display in cx.displays() {
+        ghost_shell_bar::open(display, app_config.clone(), cx)?;
     }
-
-    cx.set_global(app_config);
-    cx.set_global(app_state);
-
-    cx.on_action(|_: &Quit, cx| cx.quit());
-
-    cx.bind_keys([KeyBinding::new("cmd-q", Quit, None)]);
 
     Ok(())
 }
--- crates/ghost-shell-bar/src/lib.rs
+++ crates/ghost-shell-bar/src/lib.rs
@@ -1,12 +1,24 @@
-use gpui::{Context, IntoElement, Render, Window, div, prelude::*, rgb, rgba};
+use std::rc::Rc;
 
-pub struct Bar;
+use anyhow::{Context, Result};
+use ghost_shell_config::AppConfig;
+use gpui::{
+    App, IntoElement, PlatformDisplay, Render, Size, Window,
+    WindowBackgroundAppearance, WindowBounds, WindowHandle, WindowKind,
+    WindowOptions, div,
+    layer_shell::{Anchor, KeyboardInteractivity, Layer, LayerShellOptions},
+    point,
+    prelude::*,
+    px, rgb, rgba,
+};
 
-impl Render for Bar {
+pub struct WindowBar;
+
+impl Render for WindowBar {
     fn render(
         &mut self,
         _window: &mut Window,
-        _cx: &mut Context<Self>,
+        _cx: &mut gpui::Context<Self>,
     ) -> impl IntoElement {
         div()
             .self_flex_end()
@@ -18,5 +30,56 @@ impl Render for Bar {
             .bg(rgba(0x0000_0000))
             .text_sm()
             .child("<bar>")
+    }
+}
+
+/// Open bar for given display, based on display properties will calculate bar width
+pub fn open(
+    display: Rc<dyn PlatformDisplay>,
+    app_config: AppConfig,
+    cx: &mut App,
+) -> Result<WindowHandle<WindowBar>> {
+    let window_options = window_options(display, app_config);
+
+    cx.open_window(window_options, |_, cx| cx.new(|_| WindowBar))
+        .context("failed to open bar")
+}
+
+fn window_options(
+    display: Rc<dyn PlatformDisplay>,
+    app_config: AppConfig,
+) -> WindowOptions {
+    let app_id: String = format!("ghost-shell-{:?}", display.id());
+    let namespace: String = format!("namespace-{:?}", display.id());
+    let display_size = display.bounds().size;
+    let window_size = Size::new(display_size.width, px(app_config.bar_height));
+
+    WindowOptions {
+        window_bounds: Some(WindowBounds::Windowed(gpui::Bounds {
+            origin: point(px(0.0), px(0.0)),
+            size: window_size,
+        })),
+        titlebar: None,
+        focus: false,
+        show: true,
+        kind: WindowKind::LayerShell(LayerShellOptions {
+            namespace,
+            layer: Layer::Top,
+            anchor: Anchor::TOP | Anchor::LEFT | Anchor::RIGHT,
+            exclusive_zone: Some(px(app_config.bar_exclusive_zone)),
+            keyboard_interactivity: KeyboardInteractivity::OnDemand,
+            ..Default::default()
+        }),
+        is_movable: false,
+        app_owns_titlebar_drag: false,
+        is_resizable: false,
+        is_minimizable: false,
+        display_id: Some(display.id()),
+        window_background: WindowBackgroundAppearance::Blurred,
+        app_id: Some(app_id),
+        window_min_size: None,
+        window_decorations: None,
+        icon: None,
+        tabbing_identifier: None,
     }
 }
