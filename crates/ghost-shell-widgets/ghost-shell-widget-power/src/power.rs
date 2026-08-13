f282f1f82716e38848bd7ee0fdca0927d1a7b287
nanobreaker <nan0br3aker@gmail.com>
2026-08-13 14:36:14 +0300

    style(power): display battery levels while discharging

--- crates/ghost-shell-widgets/ghost-shell-widget-power/src/power.rs
+++ crates/ghost-shell-widgets/ghost-shell-widget-power/src/power.rs
@@ -1,7 +1,7 @@
 use anyhow::{Context as _, Result};
 use gpui::{Context, Window, div, prelude::*, px};
 use gpui_component::{ActiveTheme, Icon, Sizable};
-use starship_battery::{Battery, Manager, State};
+use starship_battery::{Battery, Manager, State, units::ratio::percent};
 
 pub struct PowerWidget {
     battery: Battery,
@@ -17,14 +17,23 @@ impl PowerWidget {
             .find(|battery| battery.model().is_some())
             .context("no suitable battery found")?;
 
+        // todo: implement a task that will check battery status every minute
+
         Ok(Self { battery })
     }
 
     fn battery_icon(&self) -> &'static str {
         match self.battery.state() {
-            State::Unknown => "icons/battery-unknown.svg",
+            State::Unknown => "icons/battery-empty.svg",
             State::Charging => "icons/battery-charging.svg",
-            State::Discharging => "icons/battery-discharging.svg",
+            State::Discharging => {
+                match self.battery.state_of_charge().get::<percent>() {
+                    level if level <= 25.0 => "icons/battery-25.svg",
+                    level if level <= 50.0 => "icons/battery-50.svg",
+                    level if level <= 75.0 => "icons/battery-75.svg",
+                    _ => "icons/battery-full.svg",
+                }
+            }
             State::Empty => "icons/battery-empty.svg",
             State::Full => "icons/battery-full.svg",
         }
@@ -42,7 +51,7 @@ impl Render for PowerWidget {
         div().id("power").flex().items_center().child(
             Icon::empty()
                 .path(icon)
-                .with_size(px(18.0))
+                .with_size(px(24.0))
                 .text_color(cx.theme().colors.foreground),
         )
     }
